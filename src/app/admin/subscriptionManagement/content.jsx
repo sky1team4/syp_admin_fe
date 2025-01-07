2d339e78c5874cdf6cfb2fb1c7d98046a51c7ca6
Munaim <abdulmunaim.2005@gmail.com>
2025-01-07 18:15:22 +0500

    subscription Management component added

---
+++ src/app/admin/subscriptionManagement/content.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import UpperSide from '../../components/upperDashbaord'
+
+function content() {
+    return (
+        <>
+            <div className='flex flex-col gap-3'>
+                <UpperSide />
+            </div>
+        </>
+    )
+}
+
+export default content
