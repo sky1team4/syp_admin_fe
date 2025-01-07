2e45235e7cdc723ae76f794e2e8bd90f1aedefcb
Muhammad Ilyas <lyasmaliki@gmail.com>
2025-01-07 13:56:30 +0500

    profile page added

---
+++ src/app/admin/dashboard/education management/page.jsx
@@ -0,0 +1,33 @@
+"use client";
+import Image from "next/image";
+import React from "react";
+import Card from "../setting/cardmanagement";
+
+const SettingContentArea = () => {
+  const cards = [
+    {
+      title: "Degree List",
+      Icon: () => <Image src="/educationmanagement/degree list.png" width={50} height={50} alt="Relationship Icon" />,
+      link: "#",
+    },
+    {
+      title: "Field of Study",
+      Icon: () => <Image src="/educationmanagement/field of study.png" width={50} height={50} alt="Education Icon" />,
+      link: "#",
+    }
+  ];
+
+  return (
+    <div className="min-h-screen w-full bg-gray-100 p-4 rounded-xl">
+      <div className="flex flex-wrap w-full gap-4 items-center justify-start">
+        {cards.map((card, index) => (
+          <div className="w-full sm:w-1/4" key={index}>
+            <Card title={card.title} Icon={card.Icon} link={card.link} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SettingContentArea;
