b174b7749dffd6ffe90235f8579319e847a905fb
Munaim <abdulmunaim.2005@gmail.com>
2025-01-09 12:48:09 +0500

    done

---
+++ src/app/admin/degreeManagement/page.jsx
@@ -0,0 +1,15 @@
+"use client"
+import React from 'react'
+import Content from './content'
+
+function page() {
+    return (
+        <>
+            <div className='flex gap-3 w-full'>
+                <Content />
+            </div>
+        </>
+    )
+}
+
+export default page
