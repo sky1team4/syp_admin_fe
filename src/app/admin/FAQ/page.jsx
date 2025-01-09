db405f40efe5502cbf33e8e19fe148152133aab2
Munaim <abdulmunaim.2005@gmail.com>
2025-01-09 14:34:56 +0500

    done

--- src/app/admin/FAQ/page.jsx
+++ src/app/admin/FAQ/page.jsx
@@ -1,28 +1,29 @@
 "use client";
 import Image from "next/image";
 import React from "react";
-// import Card from "../../components/Card";
 import Card from "../../components/cardmanagement";
 
 const SettingContentArea = () => {
   const cards = [
     {
-      title: "Degree List",
-      Icon: () => <Image src="/educationmanagement/degree list.png" width={50} height={50} alt="Relationship Icon" />,
-      link: "#",
+      title: "FAQ Category Management",
+      Icon: "/setting_icon/RelationshipManagement.png",
+      // Icon: () => <Image src="/setting_icon/RelationshipManagement.png" width={50} height={50} alt="Relationship Icon" />,
+      link: "/admin/FAQcateManagement",
     },
     {
-      title: "Field of Study",
-      Icon: () => <Image src="/educationmanagement/field of study.png" width={50} height={50} alt="Education Icon" />,
-      link: "#",
-    }
+      title: "FAQ Q&A Management",
+      Icon: "/setting_icon/RelationshipManagement.png",
+      // Icon: () => <Image src="/setting_icon/RelationshipManagement.png" width={50} height={50} alt="Relationship Icon" />,
+      link: "/admin/FAQQAcategoryManagement",
+    },
   ];
 
   return (
     <div className="min-h-screen w-full bg-gray-100 p-4 rounded-xl">
       <div className="flex flex-wrap w-full gap-4 items-center justify-start">
         {cards.map((card, index) => (
-          <div className="w-full sm:w-1/4" key={index}>
+          <div className="" key={index}>
             <Card title={card.title} Icon={card.Icon} link={card.link} />
           </div>
         ))}
