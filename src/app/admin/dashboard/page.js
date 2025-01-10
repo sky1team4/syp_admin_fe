82282ba2091efa7ea44d01c2725b8cdbad80b90b
Muhammad Ilyas <lyasmaliki@gmail.com>
2025-01-10 16:00:01 +0500

    dashbaord errors and side bar fixed

--- src/app/admin/dashboard/page.js
+++ src/app/admin/dashboard/page.js
@@ -10,10 +10,10 @@ import Notification from './notification'
 const DashboardPage = () => {
 
   const data = [
-    { id: 1, label: "Total user", value: "8,456", bgColor: "bg-purple-100", icon: "👤" },
-    { id: 2, label: "Subscribed User", value: "4,590", bgColor: "bg-red-100", icon: "📊" },
-    { id: 3, label: "Unsubscribed User", value: "3,866", bgColor: "bg-yellow-100", icon: "📄" },
-    { id: 4, label: "Active domains", value: "5,455", bgColor: "bg-green-100", icon: "🔑" },
+    { id: 1, label: "Total user", value: "8,456", bgColor: "bg-purple-100", icon:'/totalusers.png' },
+    { id: 2, label: "Subscribed User", value: "4,590", bgColor: "bg-red-100", icon: '/subscribeuser.png' },
+    { id: 3, label: "Unsubscribed User", value: "3,866", bgColor: "bg-yellow-100", icon:'/unsubscribe.png' },
+    { id: 4, label: "Active domains", value: "5,455", bgColor: "bg-green-100", icon: '/activedomain.png' },
   ];
   return (
     // <div className='flex gap-3 bg-gray-50'>
