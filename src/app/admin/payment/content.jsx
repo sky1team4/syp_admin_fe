974240a5d0eceb39f866e48f5859676f030d3cd9
Munaim <abdulmunaim.2005@gmail.com>
2025-01-11 17:44:01 +0500

    done

--- src/app/admin/payment/content.jsx
+++ src/app/admin/payment/content.jsx
@@ -25,7 +25,7 @@ const PaymentIntegration = () => {
     <div className="h-full flex flex-wrap gap-4">
       {/* <div className="flex flex-wrap w-full gap-4 items-center justify-start"> */}
         {paymentMethods.map((card, index) => (
-          <div className="" key={index}>
+          <div className="shadow rounded-xl" key={index}>
             <Card title={card.title} Icon={card.icon} link={card.link} />
           </div>
         ))}
