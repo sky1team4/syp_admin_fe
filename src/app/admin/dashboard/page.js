4d689d51846b150ed3f3e4439651a4ed79767010
Muhammad Ilyas <lyasmaliki@gmail.com>
2025-01-11 22:08:14 +0500

    no of cards fixed

--- src/app/admin/dashboard/page.js
+++ src/app/admin/dashboard/page.js
@@ -10,25 +10,22 @@ import Notification from './notification'
 const DashboardPage = () => {
 
   const data = [
-    { id: 1, label: "Total user", value: "8,456", bgColor: "bg-purple-100", icon:'/totalusers.png' },
+    { id: 1, label: "Total user", value: "8,456", bgColor: "bg-purple-100", icon: '/totalusers.png' },
     { id: 2, label: "Subscribed User", value: "4,590", bgColor: "bg-red-100", icon: '/subscribeuser.png' },
-    { id: 3, label: "Unsubscribed User", value: "3,866", bgColor: "bg-yellow-100", icon:'/unsubscribe.png' },
+    { id: 3, label: "Unsubscribed User", value: "3,866", bgColor: "bg-yellow-100", icon: '/unsubscribe.png' },
     { id: 4, label: "Active domains", value: "5,455", bgColor: "bg-green-100", icon: '/activedomain.png' },
   ];
+
   return (
-    // <div className='flex gap-3 bg-gray-50'>
-    //   <Sidebar />
-    //   <div className='flex flex-col gap-3 w-full '>
-    //     <Header />
-        <div className='flex gap-3 w-full'>
-          <div className='flex flex-col gap-3 w-auto'>
-            <UpperSide title="Today's Summary" data={data} btnText="Export" />
-            <UserTable />
-          </div>
-          <Notification />
-        </div>
-      // </div>
-    // </div>
+    <div className='flex flex-col md:flex-row gap-3 w-full p-4'>
+      {/* <Sidebar /> */}
+      <div className='flex flex-col flex-1 gap-3'>
+        {/* <Header /> */}
+        <UpperSide title="Today's Summary" data={data} btnText="Export" />
+        <UserTable />
+      </div>
+      <Notification />
+    </div>
   )
 }
 
