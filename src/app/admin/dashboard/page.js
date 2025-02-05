961519a7b556f597e3e6e76aa147e0ecd17b306b
Munaim <abdulmunaim.2005@gmail.com>
2025-02-05 18:47:31 +0500

    done

--- src/app/admin/dashboard/page.js
+++ src/app/admin/dashboard/page.js
@@ -11,7 +11,7 @@ const DashboardPage = () => {
   
 
   return (
-    <div className='flex flex-col md:flex-row gap-5 w-full p-4'>
+    <div className='flex flex-col md:flex-row gap-5 w-full md:p-4'>
       {/* <Sidebar /> */}
       <div className='flex flex-col flex-1 gap-3'>
         {/* <Header /> */}
