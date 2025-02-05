9d0dd6809f75005e1c45f4989e6005bca75d9447
Muhammad Ilyas <lyasmaliki@gmail.com>
2025-02-05 19:03:53 +0500

    subscription cross btn and company pages responsive fixed

--- src/app/admin/companyPage/page.js
+++ src/app/admin/companyPage/page.js
@@ -2,13 +2,11 @@ import React from 'react';
 
 const ComingSoon = () => {
     return (
-        <div className='ml-20 h-[28rem]' style={{ textAlign: 'center', marginTop: '50px' }}>
-            <h1 className='text-purple-600 text-[120px] font-bold'>Coming Soon</h1>
+        <div className='ml-5 md:ml-20 h-[28rem]' style={{ textAlign: 'center', marginTop: '20px' }}>
+            <h1 className='text-purple-600 text-[60px] md:text-[120px] font-bold'>Coming Soon</h1>
 
             {/* <p className='text-gray-500'>We are working hard to launch our new website. Stay tuned!</p> */}
         </div>
-
-
     );
 };
 
