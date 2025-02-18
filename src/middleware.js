910a6c9de0d45eab69d20364955347ccfd6c0b2b
Muddassir <muddassir.books@gmail.com>
2025-02-18 17:05:48 +0500

    redirect login page fixed

--- src/middleware.js
+++ src/middleware.js
@@ -4,13 +4,22 @@ export function middleware(req) {
     const token = req.cookies.get("authToken"); // Get token from cookies
     const { pathname } = req.nextUrl;
 
-    if (!token && pathname.startsWith("/admin")) {
+    // Redirect to login if token is undefined and trying to access admin routes, excluding /admin-Login
+    if (!token && pathname.startsWith("/admin") && pathname !== "/admin-Login") {
+        console.log("Redirecting to /admin-Login");
         return NextResponse.redirect(new URL("/admin-Login", req.url));
     }
+
+    // Redirect to dashboard if already logged in and trying to access login or forget-password page
+    if (token && (pathname === "/admin-Login")) {
+        console.log("Redirecting to /admin/dashboard");
+        return NextResponse.redirect(new URL("/admin/dashboard", req.url));
+    }
+
 
     return NextResponse.next();
 }
 
 export const config = {
-    matcher: ["/admin/:path*"], // Protects all admin routes
+    matcher: ["/admin/:path*", "/admin-Login", "/forget-password"], // Protects all admin routes and admin-Login
 };
