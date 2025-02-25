47a8194d221cec06fcb4a877e9ff7ca26921abbc
Munaim <abdulmunaim.2005@gmail.com>
2025-02-25 22:28:21 +0500

    forgot pass done

--- src/app/forget-password/page.js
+++ src/app/forget-password/page.js
@@ -100,9 +100,9 @@ const ForgetPassword = () => {
           {message && (
             <div 
               className={`text-sm ${
-                message.toLowerCase().includes('success') || message.toLowerCase().includes('will receive')
-                  ? 'text-green-600 bg-green-50' 
-                  : 'text-red-600 bg-red-50'
+                // message.toLowerCase().includes('success') || message.toLowerCase().includes('will receive')
+                  'text-green-600 bg-green-50' 
+                  
               } text-center p-2 rounded-md`}
             >
               {message}
