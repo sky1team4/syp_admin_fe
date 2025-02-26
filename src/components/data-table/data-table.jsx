eff54151de196d9f200f25ed6d7c7a5deb3cf0ee
Munaim <abdulmunaim.2005@gmail.com>
2025-02-26 18:32:59 +0500

    dashboard table aligen fixed

--- src/components/data-table/data-table.jsx
+++ src/components/data-table/data-table.jsx
@@ -49,13 +49,15 @@ export function DataTable({ columns, data }) {
             {table.getHeaderGroups().map((headerGroup) => (
               <TableRow key={headerGroup.id}>
                 {headerGroup.headers.map((header) => (
-                  <TableHead key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
+                  <TableHead key={header.id} className="text-left">
+                    <div className="flex justify-start">
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </div>
                   </TableHead>
                 ))}
               </TableRow>
@@ -67,59 +69,59 @@ export function DataTable({ columns, data }) {
                 <TableRow key={row.id}>
                   {row.getVisibleCells()
                     .map((cell) => (
-                      <TableCell key={cell.id}>
-                        {cell.column.id === "user_name" ? (
-                          <div className="flex items-center space-x-3">
-                            <Image
-                              src={cell.getValue()?.image || "/dash.png"}
-                              alt="Profile"
-                              width={32}
-                              height={32}
-                              className="rounded-lg"
-                            />
-                            <div>
-                              <p className="text-sm font-medium">
-                                {cell.getValue() || "Unknown"}
-                              </p>
-                              <p className="text-xs text-gray-500">
-                                {/* {cell.getValue()?.email || "No Email"} */}
-                              </p>
+                      <TableCell key={cell.id} className="text-left">
+                        <div className="flex justify-start">
+                          {cell.column.id === "user_name" ? (
+                            <div className="flex items-center space-x-3">
+                              <Image
+                                src={cell.getValue()?.image || "/dash.png"}
+                                alt="Profile"
+                                width={32}
+                                height={32}
+                                className="rounded-lg"
+                              />
+                              <div>
+                                <p className="text-sm font-medium">
+                                  {cell.getValue() || "Unknown"}
+                                </p>
+                                <p className="text-xs text-gray-500">
+                                  {/* {cell.getValue()?.email || "No Email"} */}
+                                </p>
+                              </div>
                             </div>
-                          </div>
-                        ) : cell.column.id === "phoneNumber" ? (
-                          <span className="px-2 py-1 text-xs rounded-full bg-purple-100 text-purple-500">
-                            {row.original.phoneNumber || "N/A"}
-                          </span>
-                        ) : cell.column.id === "status" ? (
-                          <span
-                            className={`text-sm font-medium  ${
-                              cell.getValue() === "active" ? "bg-green-100 text-green-600 rounded-full px-2 py-1" : "bg-yellow-100 text-yellow-600 rounded-full px-2 py-1"
-                            }`}
-                          >
-                            {cell.getValue() ? cell.getValue().toString() : "N/A"}
-                          </span>
-                        ) : cell.column.id === "billingPeriod" ? (
-                          <span
-                            className={`px-2 py-1 text-xs rounded-full ${
-                              cell.getValue() === "MONTHLY" ? "bg-green-100 text-green-600" :
-                              cell.getValue() === "YEARLY" ? "bg-blue-100 text-blue-600" :
-                              cell.getValue() === null ? "bg-yellow-100 text-yellow-600" :
-                              "bg-gray-100 text-gray-600" // Default case
-                            }`}
-                          >
-                            {cell.getValue() === "MONTHLY" ? "Monthly Subscription" :
-                             cell.getValue() === "YEARLY" ? "Yearly Subscription" :
-                             cell.getValue() === null ? "Free Member" :
-                             "N/A"
-                            }
-                          </span>
-                        ) : (
-                          flexRender(cell.column.columnDef.cell, cell.getContext())
-                        
-                        )}
+                          ) : cell.column.id === "phoneNumber" ? (
+                            <span className="px-2 py-1 text-xs rounded-full bg-purple-100 text-purple-500">
+                              {row.original.phoneNumber || "N/A"}
+                            </span>
+                          ) : cell.column.id === "status" ? (
+                            <span
+                              className={`text-sm font-medium  ${
+                                cell.getValue() === "active" ? "bg-green-100 text-green-600 rounded-full px-2 py-1" : "bg-yellow-100 text-yellow-600 rounded-full px-2 py-1"
+                              }`}
+                            >
+                              {cell.getValue() ? cell.getValue().toString() : "N/A"}
+                            </span>
+                          ) : cell.column.id === "billingPeriod" ? (
+                            <span
+                              className={`px-2 py-1 text-xs rounded-full ${
+                                cell.getValue() === "MONTHLY" ? "bg-green-100 text-green-600" :
+                                cell.getValue() === "YEARLY" ? "bg-blue-100 text-blue-600" :
+                                cell.getValue() === null ? "bg-yellow-100 text-yellow-600" :
+                                "bg-gray-100 text-gray-600" // Default case
+                              }`}
+                            >
+                              {cell.getValue() === "MONTHLY" ? "Monthly Subscription" :
+                               cell.getValue() === "YEARLY" ? "Yearly Subscription" :
+                               cell.getValue() === null ? "Free Member" :
+                               "N/A"
+                              }
+                            </span>
+                          ) : (
+                            flexRender(cell.column.columnDef.cell, cell.getContext())
+                          )}
+                        </div>
                       </TableCell>
                     ))}
-                  {/* Action Column */}
                 </TableRow>
               ))
             ) : (
