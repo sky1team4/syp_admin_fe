763996b0244dfc95541b26ae8c640018e8d818cb
Muddassir <muddassir.books@gmail.com>
2025-02-27 21:09:40 +0500

    az images

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false, // Disable Strict Mode
-    env: {
-      NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL, // Default value for production
-    },
-    images: {
-      // remotePatterns: [
-      //   {
-      //     protocol: 'http',
-      //     hostname: 'localhost',
-      //     port: '8080',
-      //     pathname: '/profile/**',
-      //   },
-      // ],
-      domains: ['localhost', process.env.NEXT_PUBLIC_API_URL], // Allow localhost and the API URL
-    },
-  };
-  
-  export default nextConfig;
-  
+  reactStrictMode: false, // Disable Strict Mode
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL, // Default value for production
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'sypadminapi.azurewebsites.net',
+        pathname: '/uploads/verifications/**', // Ensure proper path matching
+      },
+    ],
+    domains: ['localhost', 'sypadminapi.azurewebsites.net'], // Allow API hostname
+  },
+};
+
+export default nextConfig;
