631cd1382315c2474dea4b06f6492ff0ffdc6b36
Muhammad Ilyas <lyasmaliki@gmail.com>
2025-05-05 13:17:21 +0500

    search abr done

--- src/app/admin/dashboard/mianpart.jsx
+++ src/app/admin/dashboard/mianpart.jsx
@@ -9,11 +9,12 @@ import { DataTable } from "@/components/data-table/data-table"
 
 export default function DashboardPage() {
   // console.log("DashboardPage");
-  const dispatch = useDispatch(); 
+  const dispatch = useDispatch();
   const usersData = useSelector((state) => state.auth.users);
   const verificationRequestsData = useSelector((state) => state.badgeVerificationList.data);
   const [activeTab, setActiveTab] = useState("Users");
-  
+  const [searchQuery, setSearchQuery] = useState("");
+
   useEffect(() => {
     dispatch(fetchAllUsers());
     dispatch(GetAllbadgeVerificationRequest());
@@ -27,60 +28,71 @@ export default function DashboardPage() {
     setActiveTab(tab);
   };
 
-  
+  const handleSearch = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
   // const verificationRequestsData = [];
-  
+
   // console.log("Badge Verification Data", verificationRequestsData);
 
   return (
     <div >
       <div className="w-full py-10 bg-white rounded-xl p-5 shadow-md">
-      <div className="flex items-center justify-between mb-4">
-        <h2 className="text-xl md:text-2xl font-bold text-gray-800">
-          Users Information
-        </h2>
-        {/* <button className="bg-gray-100 rounded-full">
-          <Image alt="more" src="/More.svg" width={45} height={45} />
-        </button> */}
-      </div>
-      {/* Tabs */}
-      <div className="flex items-center justify-start mb-4 w-fit border border-purple-600 rounded-lg overflow-hidden">
-        <button
-          className={`px-4 py-2 text-sm md:text-base font-medium transition ${
-            activeTab === "Users"
-              ? "bg-purple-600 text-white"
-              : "bg-white text-purple-600"
-          }`}
-          onClick={() => handleTabClick("Users")}
-        >
-          Users
-        </button>
+          <h2 className="text-xl md:text-2xl mb-2 font-bold text-gray-800">
+            Users Information
+          </h2>
+         
+        {/* Tabs */}
 
-        <button
-          className={`px-4 py-2 text-sm md:text-base font-medium transition ${
-            activeTab === "Verification Requests"
-              ? "bg-purple-600 text-white"
-              : "bg-white text-purple-600"
-          }`}
-          onClick={() => handleTabClick("Verification Requests")}
-        >
-          Verification Requests
-        </button>
-      </div>
-      {activeTab === "Users" && (
-        <DataTable
-          columns={columns}
-          data={usersData}
-          onVerify={handleVerification}
-        />
-      )}
-      {activeTab === "Verification Requests" && (
-        <DataTable
-          columns={BadgeVerificationColumns}
-          data={verificationRequestsData}
-          onVerify={handleVerification}
-        />
-      )}
+        <div className="flex items-center justify-between">
+          <div className="flex items-center justify-start mb-4 w-fit border border-purple-600 rounded-lg overflow-hidden">
+            <button
+              className={`px-4 py-2 text-sm md:text-base font-medium transition ${activeTab === "Users"
+                ? "bg-purple-600 text-white"
+                : "bg-white text-purple-600"
+              }`}
+              onClick={() => handleTabClick("Users")}
+              >
+              Users
+            </button>
+
+            <button
+              className={`px-4 py-2 text-sm md:text-base font-medium transition ${activeTab === "Verification Requests"
+                ? "bg-purple-600 text-white"
+                : "bg-white text-purple-600"
+              }`}
+              onClick={() => handleTabClick("Verification Requests")}
+              >
+              Verification Requests
+            </button>
+          </div>
+           <div className="w-[20%] max-w-md">
+            <input
+              type="text"
+              value={searchQuery}
+              onChange={handleSearch}
+              placeholder="Search users..."
+              className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500"
+            />
+          </div>
+        </div>
+        {activeTab === "Users" && (
+          <DataTable
+            columns={columns}
+            data={usersData}
+            onVerify={handleVerification}
+            globalFilter={searchQuery}
+          />
+        )}
+        {activeTab === "Verification Requests" && (
+          <DataTable
+            columns={BadgeVerificationColumns}
+            data={verificationRequestsData}
+            onVerify={handleVerification}
+            globalFilter={searchQuery}
+          />
+        )}
       </div>
     </div>
   );
