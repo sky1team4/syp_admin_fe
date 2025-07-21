711edcd64679a06392a118dbda1dd5b40daca5b1
sky1team4 <sky1team4@gmail.com>
2025-07-21 14:55:02 +0500

    payment method page responsive done

--- src/app/admin/payment/content.jsx
+++ src/app/admin/payment/content.jsx
@@ -22,7 +22,7 @@ const PaymentIntegration = () => {
   ];
 
   return (
-    <div className=" flex flex-wrap gap-4">
+    <div className="flex justify-center md:justify-start flex-wrap gap-4">
       {/* <div className="flex flex-wrap w-full gap-4 items-center justify-start"> */}
       {paymentMethods.map((card, index) => (
         <div className="shadow-lg rounded-xl" key={index}>
