5f8f93603274b3e544ffe7fa5a3b382087781bf0
sky1team4 <sky1team4@gmail.com>
2025-07-21 12:51:45 +0500

    company page responsive done

--- src/app/admin/companyPage/page.js
+++ src/app/admin/companyPage/page.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ComingSoon = () => {
     return (
-        <div className='ml-5 md:ml-20 h-[28rem]' style={{ textAlign: 'center', marginTop: '20px' }}>
+        <div className='ml-5 md:ml-20 h-screen flex justify-center pt-[5rem]'>
             <h1 className='text-purple-600 text-[60px] md:text-[120px] font-bold'>Coming Soon</h1>
 
             {/* <p className='text-gray-500'>We are working hard to launch our new website. Stay tuned!</p> */}
