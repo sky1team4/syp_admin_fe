1669cf57de28eaaf6f4d674e6590be9189f256fd
sky1team4 <sky1team4@gmail.com>
2025-07-21 14:58:51 +0500

    setting page responsive done

--- src/app/admin/setting/Card.jsx
+++ src/app/admin/setting/Card.jsx
@@ -92,7 +92,7 @@ const SettingContentArea = () => {
   ];
 
   return (
-    <div className="h-full md:h-[40rem] 2xl:h-[51rem] w-full bg-white p-4 rounded-xl">
+    <div className="h-screen w-full bg-white p-4 rounded-xl">
       <div className="flex flex-wrap w-full gap-4 items-center justify-center md:justify-start">
         {cards.map((card, index) => (
           <div className="" key={index}>
