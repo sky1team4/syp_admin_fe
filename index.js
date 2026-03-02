6289635adf89dbec0f8263f649dda15c060d40d6
Sky Team <sky1team4@gmail.com>
2026-03-02 04:10:20 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const express = require('express');  
 const app = express();
 // Access the PORT from environment variables   
-const port = process.env.PORT;  
+const port = process.env.PORT;    
     
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
